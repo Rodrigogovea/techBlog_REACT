11b3ee27c57804e2c9c9719eca4debdab4962895
rodrigogovea <rodrigogovea@live.com.mx>
2024-10-28 13:01:32 -0600

    Se modifico el componente pack, y se agrego la propiedad title en el db2.js

--- src/components/Pack.jsx
+++ src/components/Pack.jsx
@@ -1,9 +1,9 @@
 function Pack({pack}) {
-    const {price, photos} = pack;
+    const { title, price, photos } = pack;
     return(
         <>
            <li className="paquetes__titulo">
-             <h4 className="no-margin">Paquete básico fotografía digital</h4>
+             <h4 className="no-margin">{title}</h4>
                     <p className="paquetes--titulo__etiqueta">Precio: <span className="paquetes--titulo__info">${price}</span></p>
 
                     <p className="paquetes--titulo__etiqueta">Fotos: <span className="paquetes--titulo__info">{photos} pz.</span></p>
--- src/data/db2.js
+++ src/data/db2.js
@@ -1,16 +1,19 @@
 export const db2 = [
     {
         id: 1,
+        title: "Paquete de fotografía digital básico",
         price: 2000,
         photos: 50
     },
     {
         id: 2,
+        title: "Paquete de fotografía digital avanzado",
         price: 3000,
         photos: 60
     },
     {
         id: 3,
+        title: "Paquete de fotografía digital completo",
         price: 4000,
         photos: 80
     }
